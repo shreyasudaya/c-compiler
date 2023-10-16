7c95779bee0abb37328f29c070ed3b537bf9a75e
Aparna2512 <132004933+Aparna2512@users.noreply.github.com>
2023-10-16 15:02:40 +0530

    input test case 4
     Validates the parsing of a simple if-else statement with variable assignments.

---
+++ Phase 2/test input/in4.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = 20, y = 10;
+    int z;
+
+    if (x < y) {
+        z = 0;
+    }
+    else {
+        z = 1;
+    }
+
+    printf("Output: %d", z);
+}
